456440ffb1a5691e38b15187d0db039077742059
bou3agga <bouagga46@gmail.com>
2022-11-26 11:33:00 +0100

    Fixing wrong Links in the response body

--- Image_Similarity.py
+++ Image_Similarity.py
@@ -10,19 +10,30 @@ from wsgiref import headers
 import DeepImageSearch
 from DeepImageSearch import Index , LoadData, SearchImage
 
-os.mkdir("Data")
 df=pd.read_csv("image-similarity-bc0atzhw1x-valentino_images.csv")
 
-for line in df["image"]:
-    filename=line.split("/")[3]+"-"+line.split("/")[-1]
-    
-    path=os.path.join("./Data/",filename)
-    URL=line
-    req=urllib.request.Request(URL,headers={"User-Agent":"Mozilla/5.0"})
-    im=urllib.request.urlopen(req).read()
-    
-    img= Image.open(io.BytesIO(im))
-    img.save(path)
+def getLink(filename):
+    res=df.loc[df['image'].str.contains(filename, case=False)]
+    return res.iloc[0,0]
 
-image_list= LoadData().from_folder(["Data"])
-Index(image_list).Start()
+if os.path.exists("Data"):
+    image_list= LoadData().from_folder(["Data"])
+    Index(image_list).Start()
+else:
+    os.mkdir("Data")
+    
+
+    for line in df["image"]: 
+        
+        filename=line.split("/")[-1]
+        
+        path=os.path.join("./Data/",filename)
+        URL=line
+        req=urllib.request.Request(URL,headers={"User-Agent":"Mozilla/5.0"})
+        im=urllib.request.urlopen(req).read()
+        
+        img= Image.open(io.BytesIO(im))
+        img.save(path)
+
+    image_list= LoadData().from_folder(["Data"])
+    Index(image_list).Start()
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import io
 import os 
 import urllib
 import importlib
+
 Image_Similarity=importlib.import_module("Image_Similarity")
 class Input (BaseModel):
     url: str
@@ -14,19 +15,21 @@ app = FastAPI()
 
 @app.post("/getLink/")
 def getImage(input: Input):
+    
     filename=input.url.split("/")[-1]
     req=urllib.request.Request(input.url,headers={"User-Agent":"Mozilla/5.0"})
     im=urllib.request.urlopen(req).read()
     path=filename
     img= Image.open(io.BytesIO(im))
-    img.save(path)
-    similar_images_dict=Image_Similarity.SearchImage().get_similar_images(image_path=Image_Similarity.image_list[0],number_of_images=10)
+    img.save(filename)
+    similar_images_dict=Image_Similarity.SearchImage().get_similar_images(image_path=filename,number_of_images=15)
     url_prefix="https://www.valentino.com/"
     pred={}
     for index in similar_images_dict :
-        image_path=similar_images_dict[index].split("/")
-        image_path=image_path[-1].split("-")
-        image_path=url_prefix+image_path[0]+"/"+image_path[-1]
+        image_path=similar_images_dict[index].split("\\")
+        image_path=image_path[-1]
+        image_path=Image_Similarity.getLink(image_path)
+        
         pred[index]=image_path
-    os.remove(path)
+    os.remove(filename)
     return pred
